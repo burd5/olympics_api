c9895ae2514f33255c1c6bad139e844459193652
burd5 <austinburdette5@gmail.com>
2023-12-10 14:23:30 -0700

    removed function test for find_medals_for

--- backend/tests/test_athlete.py
+++ backend/tests/test_athlete.py
@@ -1,7 +1,7 @@
-from api.models.athlete import Athlete
+from api.models.athlete import *
 
 def test_create_athlete_instance():
-    attrs = ['Christine Jacoba Aaftink', 'F',  21, 185, 82]
+    attrs = [5, 'Christine Jacoba Aaftink', 'F',  21, 185, 82]
     athlete = Athlete(attrs)
 
     assert isinstance(athlete, Athlete)
