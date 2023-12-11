e30efb92e982d5886d9a47ce81fd83580a75507f
burd5 <austinburdette5@gmail.com>
2023-12-10 17:08:42 -0700

    added init file and changed find_all_athletes func in country.py

--- backend/api/models/country.py
+++ backend/api/models/country.py
@@ -1,8 +1,21 @@
+import psycopg2
 class Country:
     __table__ = 'olympics'
     columns = ['team', 'noc', 'events']
 
-    def __init__(self, values):
+    def __init__(self, values, database, user):
         self.__dict__ = dict(zip(self.columns, values))
+        self.database = database
+        self.user = user
+
+    def find_all_athletes_for_country(self):
+        conn = psycopg2.connect(database=self.database, user=self.user)
+        cursor = conn.cursor()
+        statement = """select distinct id, name from olympics where team = %s;"""
+        cursor.execute(statement, (self.__dict__['team'],))
+        athletes = cursor.fetchall()
+        attrs = ['id', 'name']
+        return [dict(zip(attrs, athlete)) for athlete in athletes]
+
 
     
